3cf7443d868ae686138a6240f85b72c1a1b5dc06
LLeo <lleoem@gmail.com>
2015-08-09 17:07:57 -0400

    updated :url entry

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lleo/utils "1.0.0"
   :description "My utility functions in a library"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/lleo/clojure-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [org.clojure/clojure "1.6.0"])
