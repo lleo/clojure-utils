d5d31e08a4c4f70c067a2c27c2647ad2c4323737
LLeo <lleoem@gmail.com>
2015-08-20 22:43:39 -0400

    added mimimal tests

--- test/lleo/utils_test.clj
+++ test/lleo/utils_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [lleo.utils :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def STR "foo\nbar\n")
+
+(deftest t-substr
+  "test substr"
+  (is (= "foo" (substr STR 0 3))))
+
+(deftest t-split-fixed
+  "test split-fixed"
+  (is (= 3 (count (split-fixed STR "\n")))))
+
+(deftest t-split-fixed-limit
+  "test split-fixed-limit"
+  (is (= 2 (count (split-fixed-limit STR "\n" 2)))))
+
+(deftest t-split--fixed
+  "test multimethod split \"foo\\nbar\\n\" \"\\n\""
+  (is (= 3 (count (split STR "\n")))))
+
+(deftest t-split--fixed-limit
+  "test multimethod split \"foo\\nbar\\n\" \"\\n\""
+  (is (= 2 (count (split STR "\n" 2)))))
+
+(deftest t-split--regex
+  "test multimethod split \"foo\\nbar\\n\" #\"\\n\""
+  (is (= 3 (count (split STR #"\n")))))
+
+(deftest t-split--regex-limit
+  "test multimethod split \"foo\\nbar\\n\" #\"\\n\""
+  (is (= 2 (count (split STR #"\n" 2)))))
